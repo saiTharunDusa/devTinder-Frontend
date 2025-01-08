66ab09c7ca8d4fc7dd1418cbc7a070f1335b47ce
Sai Tharun Dusa <sd2672@nau.edu>
2025-01-08 16:59:46 -0700

    Created Redux Store and integrated login API

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,25 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Body from "./components/Body"
 import Login from "./components/login"
 import Profile from "./components/profile"
+import { Provider } from "react-redux"
+import appStore from "./utils/appStore"
+import Feed from "./components/Feed"
 
 function App() {
 
 	return (
 		<>
-			<BrowserRouter basename="/">
-				<Routes>
-					<Route path="/" element={<Body />}>
-						<Route path="/login" element={<Login />} />
-						<Route path="/profile" element={<Profile />} />
-					</Route>
-				</Routes>
-			</BrowserRouter>
+			<Provider store={appStore}>
+				<BrowserRouter basename="/">
+					<Routes>
+						<Route path="/" element={<Body />}>
+							<Route path="/" element={<Feed />} />
+							<Route path="/login" element={<Login />} />
+							<Route path="/profile" element={<Profile />} />
+						</Route>
+					</Routes>
+				</BrowserRouter>
+			</Provider>
 		</>
 	)
 }
