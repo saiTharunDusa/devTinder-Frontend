60975707aa8bb274ad8ad67d106b0f9b0eb4074c
Sai Tharun Dusa <sd2672@nau.edu>
2025-01-07 22:31:48 -0700

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Body from "./components/Body"
+import Login from "./components/login"
+import Profile from "./components/profile"
 
 function App() {
 
-  return (
-    <>
-      <h1>Hello World!</h1>
-    </>
-  )
+	return (
+		<>
+			<BrowserRouter basename="/">
+				<Routes>
+					<Route path="/" element={<Body />}>
+						<Route path="/login" element={<Login />} />
+						<Route path="/profile" element={<Profile />} />
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</>
+	)
 }
 
 export default App
