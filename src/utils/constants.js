9d29689601128bcdcc42f84135a587aec5874067
Sai Tharun Dusa <sd2672@nau.edu>
2025-01-11 18:26:59 -0700

    Changed the base url to /api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:3000"
+export const BASE_URL = "/api"
 
 export const PHOTO_URL = "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
