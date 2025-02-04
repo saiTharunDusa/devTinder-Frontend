0d51fc62920caddad454b0871581193975024e4c
Sai Tharun Dusa <sd2672@nau.edu>
2025-02-03 23:23:34 -0700

    Changed the BASE_URL

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "/api"
+export const BASE_URL = location.hostname === "localhost" ? "http://localhost:3000" : "/api";
 
 export const PHOTO_URL = "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
